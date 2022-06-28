49f451ea7dcb0ba0d091097454e591d90ca44d7e
Erick Garcia Estrada <ergarcia@coosalud.com>
2022-06-28 08:16:37 -0500

    Updated controller of detallePais method

--- src/main/java/com/springboot/app/paises/controllers/PaisController.java
+++ src/main/java/com/springboot/app/paises/controllers/PaisController.java
@@ -13,7 +13,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,16 +53,16 @@ public class PaisController {
 	@GetMapping("/api/v1/countries/{regionName}")
 	@Cacheable("addresses")
 	public Collection<PaisDTO> detallePais(@RequestBody @Validated @PathVariable String regionName, 
-			@RequestHeader("host") String host,
 			HttpServletRequest request) {
 		
 		String endpiont = request.getRequestURI(); 
+		String remoteAddr = request.getRemoteAddr();
 		Date date = new Date();
 		LogConsultas log = new LogConsultas(
 				date, 
 				date, 
 				endpiont, 
-				host
+				remoteAddr
 				);
 		logConsultasService.save(log);
 		return paisService.findAllById(regionName);
